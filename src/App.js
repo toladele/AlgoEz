eb2221d33b5a8bde5830aae718e30b3b6b8046bc
toladele <tommy.oladele@mail.utoronto.ca>
2020-06-07 00:21:23 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import NavBar from './components/NavBar/NavBar';
+
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavDropdown, Navbar, Nav } from 'react-bootstrap';
+
+
+
+const NavBar = () => {
+    return (
+        <Navbar bg="primary" variant="dark">
+            <Navbar.Brand>Algo-Easy</Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse className="justify-content-end">
+                <Nav>
+                    <Nav.Link href="/">Place Start !</Nav.Link>
+                    <Nav.Link href="/">Place Target !</Nav.Link>
+                    <NavDropdown title="Path Finding Algorithms" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="/">Algo1</NavDropdown.Item>
+                        <NavDropdown.Item href="/">Algo2</NavDropdown.Item>
+                        <NavDropdown.Item href="/">Algo3</NavDropdown.Item>
+                        <NavDropdown.Item href="/">Algo4</NavDropdown.Item>
+                        <NavDropdown.Item href="/">Algo5</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="Sorting Algorithms" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="/">Algo1</NavDropdown.Item>
+                        <NavDropdown.Item href="/">Algo2</NavDropdown.Item>
+                        <NavDropdown.Item href="/">Algo3</NavDropdown.Item>
+                        <NavDropdown.Item href="/">Algo4</NavDropdown.Item>
+                        <NavDropdown.Item href="/">Algo5</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+}
+
+
+
+export default NavBar
