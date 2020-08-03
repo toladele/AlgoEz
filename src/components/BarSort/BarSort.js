046e3d0057a297c218e8e93f832b119eb90dc039
Mathuran-Mang <45081636+Mathuran-Mang@users.noreply.github.com>
2020-08-03 19:58:17 -0400

    Update BarSort.js

--- src/components/BarSort/BarSort.js
+++ src/components/BarSort/BarSort.js
@@ -85,6 +85,41 @@ class BarSort extends React.Component {
             }
         }
     }
+    merge_sort_aux(arr1, arr2) {
+        var arr_final = [];
+          while (arr1.length != 0 && arr2.length != 0) {
+        
+            if (arr1[0] <= arr2[0]) {
+        
+              arr_final.push(arr1[0]);
+              arr1 = arr1.slice(1);
+              //slice to get single elements to compare  
+            } 
+            else {
+        
+            arr_final.push(arr2[0]);
+            arr2 = arr2.slice(1)
+             }
+        }
+          while (arr1.length)
+            arr_final.push(arr1.shift());
+          while (arr2.length)
+            arr_final.push(arr2.shift());
+          return arr_final;
+        }
+        merge_sort(a) {
+          if (a.length <= 1) {
+        
+            return a; }
+          else {
+            var mid = parseInt(a.length / 2);
+            var arr1   = a.slice(0, mid);
+            var arr2  = a.slice(mid, arr.length);
+        
+            return merge_sort_aux(merge_sort(arr1), merge_sort(arr2));
+            //recursive call
+          }
+        }
     swap(array, leftIndex, rightIndex){
         var temp = array[leftIndex];
         array[leftIndex] = array[rightIndex];
@@ -123,6 +158,9 @@ class BarSort extends React.Component {
         }
         return array;
     }
+
+    
+    
     one() {
         return new Promise(resolve => {
           console.log("one");
